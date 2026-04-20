6fcfb3d42575bb94f5c212e95f3a0ca5a1f3f1fe
Atom Roese <40679658+ARoese@users.noreply.github.com>
2026-04-20 02:19:18 -0400

    proper unregister and 1.1.1

--- __init__.py
+++ __init__.py
@@ -7,19 +7,17 @@ from .ops.painting import PaintMaterial, MaterialSwitcherPanel
 bl_info = {
     "name": "HD2 LUT Visual Edit",
     "blender": (4, 3, 0),
-    "version": (1, 1, 0),
+    "version": (1, 1, 1),
     "category": "Material",
 }
     
-def add_make_editable_context():
-    def draw_menu(self: bpy.types.Menu, context):
-        assert self.layout is not None
-        layout = self.layout
-        layout.operator_context = 'INVOKE_DEFAULT'
-        layout.operator(MakeEditableOperator.bl_idname, text="Make Editable")
-        layout.operator(ExportToArrayOperator.bl_idname, text="Export to IDMask Array")
-
-    bpy.types.NODE_MT_context_menu.append(draw_menu)
+def draw_node_menu(self: bpy.types.Menu, context):
+    assert self.layout is not None
+    layout = self.layout
+    layout.separator(type="LINE")
+    layout.operator_context = 'INVOKE_DEFAULT'
+    layout.operator(MakeEditableOperator.bl_idname, text="Make Editable")
+    layout.operator(ExportToArrayOperator.bl_idname, text="Export to IDMask Array")    
 
 def register():
     print("registered visual edit addon")
@@ -27,13 +25,14 @@ def register():
     bpy.utils.register_class(ExportToArrayOperator)
     bpy.utils.register_class(PaintMaterial)
     bpy.utils.register_class(MaterialSwitcherPanel)
-    add_make_editable_context()
+    bpy.types.NODE_MT_context_menu.append(draw_node_menu)
 
 def unregister():
     bpy.utils.unregister_class(MakeEditableOperator)
     bpy.utils.unregister_class(ExportToArrayOperator)
     bpy.utils.unregister_class(PaintMaterial)
     bpy.utils.unregister_class(MaterialSwitcherPanel)
+    bpy.types.NODE_MT_context_menu.remove(draw_node_menu)
 
 if __name__ == "__main__":
     register()
