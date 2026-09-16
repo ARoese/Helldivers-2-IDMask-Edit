6e2707cd5427905fd5511723fb37166c6098ab9b
Atom Roese <40679658+ARoese@users.noreply.github.com>
2026-09-16 15:01:14 -0400

    v1.5.5

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ from .ops.pillow_install import LibsInstallPanel, InstallLibsOperator
 bl_info = {
     "name": "HD2 LUT Visual Edit",
     "blender": (4, 3, 0),
-    "version": (1, 5, 4),
+    "version": (1, 5, 5),
     "category": "Material",
 }
 
